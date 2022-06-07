2d1707f5797688e381ad85bed378a3f8212b566e
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-06-06 23:01:46 -0300

    Adicionado animações ao texto da navbar e ao botão

---
+++ JS/script.js
@@ -0,0 +1,34 @@
+/* animações text nav-bar */
+
+/* Text GitHub */
+function mouseEnterMouse1(){
+    document.getElementById("github").style = "color: gray;";
+}
+function mouseLeaveMouse1(){
+    document.getElementById("github").style = "color: white;";
+}
+/* Text Linkedin */
+function mouseEnterMouse2(){
+    document.getElementById("linkedin").style = "color: gray;";
+}
+function mouseLeaveMouse2(){
+    document.getElementById("linkedin").style = "color: white;";
+}
+
+
+/* animações botão Logar */
+document.querySelector("button").style = " color: white;margin-top: 5%;border-radius: 5px;height: 50px;font-size: 16px;font-weight: bold;font-family: 'Tiro Devanagari Sanskrit', serif;background-image: linear-gradient(to right, rgb(58, 11, 119), rgb(4, 0, 36) );border: none;";
+
+function clickButton(){
+    document.querySelector("button").style = " color: black;margin-top: 5%;border-radius: 5px;height: 50px;font-size: 16px;font-weight: bold;font-family: 'Tiro Devanagari Sanskrit', serif;background-image: linear-gradient(to right, rgb(96, 1, 219), rgb(9, 5, 37) );border: none;";
+}
+function unClickButton(){
+    document.querySelector("button").style = " color: white;margin-top: 5%;border-radius: 5px;height: 50px;font-size: 16px;font-weight: bold;font-family: 'Tiro Devanagari Sanskrit', serif;background-image: linear-gradient(to right, rgb(58, 11, 119), rgb(4, 0, 36) );border: none;";
+}
+function mouseEnterMouse(){
+    document.querySelector("button").style = " color: black;margin-top: 5%;border-radius: 5px;height: 50px;font-size: 16px;font-weight: bold;font-family: 'Tiro Devanagari Sanskrit', serif;background-image: linear-gradient(to right, rgb(58, 11, 119), rgb(4, 0, 36) );border: none;";
+}
+function mouseLeaveMouse(){
+    document.querySelector("button").style = " color: white;margin-top: 5%;border-radius: 5px;height: 50px;font-size: 16px;font-weight: bold;font-family: 'Tiro Devanagari Sanskrit', serif;background-image: linear-gradient(to right, rgb(58, 11, 119), rgb(4, 0, 36) );border: none;";
+}
+
